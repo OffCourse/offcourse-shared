944d4725d9c283842720f5dafecd677a713af1c4
Jan Hein Hoogstad <yeehaa@offcourse.io>
2016-10-09 23:33:28 +0200

    add tags to resource spec

--- src/shared/specs/resource.cljs
+++ src/shared/specs/resource.cljs
@@ -7,10 +7,12 @@
 (spec/def ::content (spec/nilable string?))
 (spec/def ::description (spec/nilable string?))
 (spec/def ::resource-type string?)
+(spec/def ::tags (spec/* string?))
 
 (spec/def ::resource (spec/keys :req-un [::resource-url
                                          ::resource-type
                                          ::description
+                                         ::tags
                                          ::content]))
 
 (spec/def ::resources (spec/* ::resource))
