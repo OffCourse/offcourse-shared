a08e95e932919ee45f22a5ff5f3644ea938e70a6
Jan Hein Hoogstad <yeehaa@offcourse.io>
2016-11-11 00:08:34 +0100

    small update

--- src/shared/specs/github.cljs
+++ src/shared/specs/github.cljs
@@ -2,14 +2,17 @@
   (:require [cljs.spec :as spec]
             [shared.specs.base :as base]))
 
-(spec/def ::sha string?)
 (spec/def ::owner string?)
 (spec/def ::name string?)
 (spec/def ::mode string?)
 (spec/def ::type string?)
 (spec/def ::size int?)
+
+(spec/def ::sha string?)
 (spec/def ::url ::base/url)
 
-(spec/def ::repo   (spec/keys :req-un [::name ::owner ::sha]))
+(spec/def ::repo   (spec/keys :req-un [::name ::owner ::sha]
+                              :opt-un [::base/user-name]))
+
 (spec/def ::path   string?)
-(spec/def ::course (spec/keys :req-un [::path ::sha ::url]))
+(spec/def ::course (spec/keys :req-un [::course-path ::sha ::url]))
